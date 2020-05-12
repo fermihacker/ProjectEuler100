4c3ead1538395dab89ee014c3542854bff151ba5
fermihacker <57656949+fermihacker@users.noreply.github.com>
2020-05-12 17:03:54 +0530

    Problem 7

--- Problem007.py
+++ Problem007.py
@@ -1,3 +1,5 @@
+from time import time
+
 def check(n):
     for i in range(n-1,1,-1):
         if n%i==0:
@@ -12,4 +14,7 @@ def nth_prime(n):
         i+=1
     return s[-1]
 
-nth_prime(10001)
+if __name__ == '__main__':
+    start = time()
+    print("Answer:{}".format(nth_prime(10001)))
+    print("Time Taken:{}".format(time()-start))
